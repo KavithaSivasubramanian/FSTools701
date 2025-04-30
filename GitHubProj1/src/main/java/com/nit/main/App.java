26b8bfea6a443b15f04b378ef52d122f7c3cec13
mselv <mselv@Akshaydev>
2025-04-30 09:37:26 -0400

    task 1 completed by developer

--- GitHubProj1/src/main/java/com/nit/main/App.java
+++ GitHubProj1/src/main/java/com/nit/main/App.java
@@ -6,5 +6,6 @@ package com.nit.main;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("First GitHub Project");
     }
 }
